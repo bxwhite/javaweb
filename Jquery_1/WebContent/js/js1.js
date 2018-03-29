5896b0c427b148c189f67a5d9fdbeec6302bfcd0
bxwhite <1906987929@qq.com>
2018-03-29 16:13:57 +0800

    jquery

---
+++ Jquery_1/WebContent/js/js1.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+$(document).ready(function (){
+	/*$("#bid").click(function () {
+		alert("111");
+	});*/
+	/*$("#bid").bind("click", clickHandler1);*/
+	/*$("#bid").click(function() {
+		alert($("#pid").text());
+	});*/
+	/*$("#bid").click(function() {
+		alert($("#pid").html());
+	});*/
+	$("#bid").click(function() {
+		$("#pid").append(" nihao");
+	
+});
+	
+});	
+ /*function clickHandler1() {
+	console.log("click1");
+	
+}*/
+	
